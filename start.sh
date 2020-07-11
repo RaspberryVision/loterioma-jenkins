bc79b7bc57b22b5fa4352e9b1f0e00cb1da7e315
Rafal Malik <rafalmalik.info@gmail.com>
2020-07-11 14:41:14 +0200

    Updated paths.

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Running Jenkins container via docker
-docker run -d -p 5549:8080 -p 50000:50000 -v /var/www/html/respberryvision:/var/jenkins_home/workspace \
-       -v /var/applications/loterioma/jenkins:/var/jenkins_home \
+docker run -d -p 5549:8080 -p 50000:50000 \
+       -v /var/www/html/respberryvision/loterioma/jenkins-workspace:/var/jenkins_home/workspace \
+       -v /var/www/html/raspberryvision/loterioma/jenkins:/var/jenkins_home \
        -v /var/run/docker.sock:/var/run/docker.sock \
        -v $(which docker):/usr/bin/docker \
        -e JENKINS_USER=$(id -u) jenkins/jenkins:lts
